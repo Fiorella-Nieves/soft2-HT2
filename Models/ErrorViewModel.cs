da8e0fce0b60f02fcdfbd95eda5b6df18a595a70
Fiorella <fiorella.camayoc@gmail.com>
2026-05-17 15:14:00 -0500

    ordenado

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Nueva_carpeta.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
